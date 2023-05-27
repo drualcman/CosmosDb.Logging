acf921ffa9ec1d129c282714f28bc476f86bec7d
Sergi Ortiz Gómez <drualcman@msn.com>
2023-05-28 07:03:05 +0800

    disable nulable

---
+++ CosmosDb.Logging/Entities/Item.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace CosmosDb.Logging.Entities;
+internal class Item
+{
+                    
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+              
+    [JsonProperty(PropertyName = "partitionKey")]
+    public string PartitionKey { get; set; }
+    public decimal Money { get; set; }
+
+    public bool Bboolean { get; set; }
+
+    public string[] Set { get; set; }
+
+    public double Numbers { get; set; }
+
+    public int Morenumbers { get; set; }
+
+    public List<Product> Onetomany { get; set; }
+}
---
+++ CosmosDb.Logging/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace CosmosDb.Logging.Entities;
+internal class Product
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    [JsonProperty(PropertyName = "categoryId")]
+    public string CategoryId { get; set; }
+
+    public double Price { get; set; }
+
+    public string[] Tags { get; set; }
+
+    [JsonProperty(PropertyName = "ttl", NullValueHandling = NullValueHandling.Ignore)]
+    public int? TimeToLive { get; set; }
+}
